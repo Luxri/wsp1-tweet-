e45ecc40376535bae0252133f4149074298e5ef4
Luxri <lucas.nasstrom@elev.ga.ntig.se>
2019-09-19 11:00:05 +0200

    Twitter looks more like Twitter now

--- tweet.php
+++ tweet.php
@@ -1,8 +1,9 @@
-
 <?php
+// tweet.php?id=3
+// tweet.php?id=<script>alert("xss")</script>
+//$tweetId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+$tweetId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 include 'include/dbinfo.php';
-// mysql -u -p
-// use database;
 try {
     $dbh = new PDO(
         'mysql:host=localhost;dbname=' . $database . '',
@@ -13,17 +14,11 @@ try {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-// select * from tweet
 $sth = $dbh->prepare('SELECT tweet.*, users.name FROM tweet
             JOIN users
             ON tweet.user_id = users.id
-            ORDER BY updated_at DESC');
+            WHERE tweet.id =' . $tweetId);
 $sth->execute();
-$result = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-include 'views/index_layout.php';
+$result = $sth->fetch(PDO::FETCH_ASSOC);
+include 'views/tweet_layout.php';
 ?>
-
-
-
-  
